20545d2bfe30e485e12bf51dd1a655f6110e7468
Sami Khan <samikhan@iut-dhaka.edu>
2024-04-26 16:03:03 +0600

    search nearby by place id and proximity added

--- src/actions/geoapify.ts
+++ src/actions/geoapify.ts
@@ -7,11 +7,16 @@ export async function searchNearby(lat:string, lon:string) {
     console.log(process.env.GEOAPIFY_API_KEY)
     const response = await fetch(`https://api.geoapify.com/v2/places?categories=tourism.information,tourism.attraction&filter=circle:${lat},${lon},50000&bias=proximity:${lat},${lon}&limit=20&apiKey=`+process.env.GEOAPIFY_API_KEY, requestOptions)
     const result = await response.json()
-    const var1 = result.features.map(function(f:any){return f.properties}) 
-    console.log(var1)
-    return var1
+    return result.features.map(function(f:any){return f.properties}) 
 }
 
+export async function searchNearbyWithinCity(lat:string, lon:string, place_id:string) {
+    var requestOptions = {
+        method: 'GET',
+    };
+    const response = await fetch(`https://api.geoapify.com/v2/places?categories=tourism.attraction&filter=place:${place_id}&bias=proximity:${lat},${lon}&limit=20&apiKey=`+process.env.GEOAPIFY_API_KEY, requestOptions)
 
-
+    const result = await response.json()
+    return result.features.map(function(f:any){return f.properties})
+}
 
--- src/app/test2/page.tsx
+++ src/app/test2/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import { searchNearby } from '@/actions/geoapify';
+import { searchNearby, searchNearbyWithinCity } from '@/actions/geoapify';
 export default function Test() {
 
   const [attraction, setAttraction] = useState([]);
   useEffect(() => {
-    searchNearby("90.3825171","23.9452074")
+    searchNearbyWithinCity("90.34686535627966", "23.81864335",    "516c2a4c40cf9956405938872a3fa9c83740f00101f901d17dd00000000000c0020892031fe0a6a2e0a6bee0a695e0a6be20e0a6aee0a6b9e0a6bee0a6a8e0a697e0a6b0")
       .then((result) => {
         console.log("result");
         setAttraction(result)
