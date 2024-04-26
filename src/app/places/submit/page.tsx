bb96522a2add434f7538514add18e72ffb13ba92
renzhamin <renzhamin@gmail.com>
2024-04-27 02:10:20 +0600

    add city selection in submit places page

--- src/app/places/submit/page.tsx
+++ src/app/places/submit/page.tsx
@@ -4,18 +4,20 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
+import { getLocations } from "@/actions/location-auto-complete"
 import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { getLocations } from "@/actions/location-auto-complete"
+import { Search } from "lucide-react"
+import { useState } from "react"
+import { FaSearch } from "react-icons/fa"
 
 const formSchema = z.object({
   name: z.string().min(2, {
@@ -24,15 +26,15 @@ const formSchema = z.object({
   street: z
     .string()
     .min(5, { message: "Street must be at least 5 characters" }),
-  postcode: z.number(),
-  lat: z
+  postcode: z.coerce.number(),
+  lat: z.coerce
     .number()
     .max(90, { message: "latitude must be between 90 and -90" })
-    .min(90, { message: "latitude must be between 90 and -90" }),
-  lon: z
+    .min(-90, { message: "latitude must be between 90 and -90" }),
+  lon: z.coerce
     .number()
-    .max(180, { message: "latitude must be between 180 and -180" })
-    .min(-180, { message: "latitude must be between 180 and -180" }),
+    .max(180, { message: "longitude must be between 180 and -180" })
+    .min(-180, { message: "longitude must be between 180 and -180" }),
 })
 
 export default function Page() {
@@ -67,94 +69,148 @@ export default function Page() {
   }
 
   return (
-    <div className="flex justify-center p-2">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <div className="flex items-center gap-2">
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                </div>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+    <div className="flex flex-col items-center justify-center gap-2 p-2">
+      {locations && selectedLocationIndex >= 0 && (
+        <p>City: {locations[selectedLocationIndex].formatted}</p>
+      )}
+      {selectedLocationIndex === -1 && (
+        <div className="flex gap-2">
+          <div className="relative">
+            <form onSubmit={onCitySubmit} className="flex gap-2">
+              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                value={city}
+                onChange={(e) => {
+                  setCity(e.currentTarget.value)
+                }}
+                placeholder="Enter city name or lat,lon"
+                className="pl-8 sm:w-[300px] md:w-[500px]"
+              />
+              <Button type="submit">
+                <FaSearch className="mr-2" />
+                Search
+              </Button>
+            </form>
+          </div>
+        </div>
+      )}
+      {cityLoading && (
+        <p className="mx-auto animate-pulse">Loading Result...</p>
+      )}
+      {locations && selectedLocationIndex == -1 && (
+        <div className="flex flex-col gap-2">
+          <p>Select a city:</p>
+          <div className="flex flex-col gap-2">
+            {locations.map((city: any, ind: number) => {
+              return (
+                <button
+                  key={ind}
+                  className="flex w-min bg-primary-foreground px-2"
+                  onClick={() => {
+                    setSelectedLocationIndex(ind)
+                    setCity(city.formatted)
+                  }}
+                >
+                  <p className="w-[300px] px-4 py-2 md:w-[600px]">
+                    {city.formatted}
+                  </p>
+                </button>
+              )
+            })}
+          </div>
+        </div>
+      )}
 
-          <FormField
-            control={form.control}
-            name="street"
-            render={({ field }) => (
-              <FormItem>
-                <div className="flex items-center gap-2">
-                  <FormLabel>Street</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                </div>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+      {selectedLocationIndex !== -1 && (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex items-center gap-2">
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="postcode"
-            render={({ field }) => (
-              <FormItem>
-                <div className="flex items-center gap-2">
-                  <FormLabel>Postcode</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                </div>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="street"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex items-center gap-2">
+                    <FormLabel>Street</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="lat"
-            render={({ field }) => (
-              <FormItem>
-                <div className="flex items-center gap-2">
-                  <FormLabel>Latitude</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                </div>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="postcode"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex items-center gap-2">
+                    <FormLabel>Postcode</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="lon"
-            render={({ field }) => (
-              <FormItem>
-                <div className="flex items-center gap-2">
-                  <FormLabel>Longitude</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                </div>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="lat"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex items-center gap-2">
+                    <FormLabel>Latitude</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <Button type="submit" className="w-full">
-            Submit
-          </Button>
-        </form>
-      </Form>
+            <FormField
+              control={form.control}
+              name="lon"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex items-center gap-2">
+                    <FormLabel>Longitude</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="w-full">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      )}
     </div>
   )
 }
