8722f282d06345bc528ee82826cb31b805c70cc2
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2024-04-26 23:58:19 +0600

    added on hover effects

--- src/components/places.tsx
+++ src/components/places.tsx
@@ -10,15 +10,15 @@ export default function Places({ places }: { places: any }) {
           places.map((place: any, index: number) => {
             if (!place.name) return <div key={index}></div>
             return (
-              <Card className="m-4" key={index}>
+              <Card className="m-4 hover:scale-105 transition-transform duration-200" key={index}>
                 <CardHeader>
-                  <h2>{place.name}</h2>
+                  <h2 className="font-bold">{place.name}</h2>
                 </CardHeader>
                 <CardContent>TODO: Some Description</CardContent>
                 <CardFooter className="text-sm text-gray-500">
                   {place.address_line2}
                 </CardFooter>
-                <CardFooter className="text-sm text-gray-500">
+                <CardFooter className="italic text-sm text-gray-500">
                   Category: {place.categories[0]}
                 </CardFooter>
               </Card>
