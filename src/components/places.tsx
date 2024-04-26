83c90f9cc743e6adf25d28ea234a2298403a6489
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2024-04-26 22:01:07 +0600

    added card for places

--- src/components/places.tsx
+++ src/components/places.tsx
@@ -1,14 +1,22 @@
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card"
+
 export default function Places({ places }: { places: any }) {
+  console.log(places)
   return (
     <div>
       <p>List of places:</p>
       <div>
         {places &&
           places.map((place: any) => {
+            if (!place.name) return <div></div>
             return (
-              <div>
-                <p>{place.name}</p>
-              </div>
+              <Card>
+                <CardHeader>
+                  <h2>{place.name}</h2>
+                </CardHeader>
+                <CardContent></CardContent>
+                <CardFooter>{place.address_line2}</CardFooter>
+              </Card>
             )
           })}
       </div>
