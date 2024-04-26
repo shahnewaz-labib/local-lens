c21560b9dddc2a382d217967e1a143a6b932b69f
Sami Khan <samikhan@iut-dhaka.edu>
2024-04-26 15:09:46 +0600

    fix conflict app/test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import SearchNearbyAttractions from "@/components/search-nearby-attractions"
 
 export default function Test() {
@@ -7,17 +6,4 @@ export default function Test() {
       <SearchNearbyAttractions />
     </div>
   )
-=======
-import { searchNearby } from "@/actions/geoapify" ;
-
-
-export default async function Test() {
-    const results = await searchNearby("90.3825171","23.9452074")
-    console.log(results)
-    return (
-        <div>
-            <p>Test</p>
-        </div>
-    )
->>>>>>> e3c0a1b (nearby places action)
 }
