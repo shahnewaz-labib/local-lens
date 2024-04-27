f8d66ff7c5cd7a02067418987e59b88e22c5e2bb
renzhamin <renzhamin@gmail.com>
2024-04-27 09:23:21 +0600

    cached city suggestions

--- src/actions/location-auto-complete.ts
+++ src/actions/location-auto-complete.ts
@@ -1,12 +1,23 @@
 "use server"
 
+import { redis } from "@/config/redis"
+
 const geoApiFiKey = process.env.GEOAPIFY_API_KEY!
 
 export async function getLocations(locationString: string) {
   if (!locationString || locationString.length === 0) return []
+  const key = `locations-${locationString}`
+  const cached = await redis.get(key)
+  if (cached) {
+    return cached
+  }
   const res = await fetch(
     `https://api.geoapify.com/v1/geocode/autocomplete?text=${locationString}&format=json&apiKey=${geoApiFiKey}`,
   )
+  if (!res.ok) {
+    return []
+  }
   const data = await res.json()
+  await redis.set(key, data.results)
   return data.results
 }
