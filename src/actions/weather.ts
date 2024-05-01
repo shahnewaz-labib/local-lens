8b3341b5424439c441452c5bb17c7f680ff63a78
renzhamin <renzhamin@gmail.com>
2024-05-01 12:33:18 +0600

    added env example

--- src/actions/weather.ts
+++ src/actions/weather.ts
@@ -1,4 +1,6 @@
-const aqiApiKey = process.env.NEXT_PUBLIC_AQI_API_KEY
+"use server"
+
+const aqiApiKey = process.env.AQI_API_KEY
 
 export async function getWeather(lat: number, lon: number) {
   const forcastDays = 3
